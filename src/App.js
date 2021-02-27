e14695aa9d824dc4a39215fd164aa63a08d3377c
Frank Corona Prendes (@chaplindev) <chaplin.developer@gmail.com>
2021-02-27 15:54:17 -0500

    Basics for React hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
+import { useState, useEffect } from "react";
 import "./styles.css";
 import logo from "./logo.svg";
 import "./App.css";
 import HelloWorld from "./components/Hello";
+import { Toast } from "react-bootstrap";
 
 export default function App() {
+  // hook useState
+  const [showToast, setShowToast] = useState(false);
+
+  // handling with useState hook
+  const toggleShowA = () => {
+    setShowToast((prevValue) => !prevValue);
+    // setShowToast(!showToast);
+  };
+
+  useEffect(() => {
+    // will be executed always after a chonge of the input selected
+    console.log(`Values updated to ${showToast}`);
+  }, [showToast]);
+
   const userInfo = {
     name: "Frank David",
     age: 32
   };
-  const hi = (name) => {
-    console.log(`Hello ${name}`);
+
+  const hi = () => {
+    toggleShowA();
   };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <HelloWorld userInfo={userInfo} hiFn={hi} />
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <HelloWorld userInfo={userInfo} hiFn={hi} />
+          <br />
+          <p>The state of the toast is: {showToast ? "Active" : "Inactive"}</p>
+
+          <Toast show={showToast} onClose={toggleShowA}>
+            <Toast.Header>
+              <strong className="mr-auto">Bootstrap</strong>
+              <small>11 mins ago</small>
+            </Toast.Header>
+            <Toast.Body>Hello, {userInfo.name}.</Toast.Body>
+          </Toast>
+        </header>
+      </div>
+    </>
   );
 }
