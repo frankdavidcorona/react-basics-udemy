d3446b1f3319a679b9951a156c30c8d6563387ca
Frank Corona Prendes (@chaplindev) <chaplin.developer@gmail.com>
2021-03-03 12:52:07 -0500

    Added support for Chart.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,61 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import moment from "moment";
+import Chart from "chart.js";
 import "./styles.css";
 import logo from "./logo.svg";
 import "./App.css";
 import HelloWorld from "./components/Hello";
-import { Toast } from "react-bootstrap";
 
 export default function App() {
-  // hook useState
-  const [showToast, setShowToast] = useState(false);
-
-  // handling with useState hook
-  const toggleShowA = () => {
-    setShowToast((prevValue) => !prevValue);
-    // setShowToast(!showToast);
-  };
-
-  useEffect(() => {
-    // will be executed always after a chonge of the input selected
-    console.log(`Values updated to ${showToast}`);
-  }, [showToast]);
-
   const userInfo = {
     name: "Frank David",
     age: 32
   };
-
-  const hi = () => {
-    toggleShowA();
+  const hi = (name) => {
+    console.log(`Hello ${name}`);
+    // setShowToast(!showToast);
   };
 
-  return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <HelloWorld userInfo={userInfo} hiFn={hi} />
-          <br />
-          <p>The state of the toast is: {showToast ? "Active" : "Inactive"}</p>
+  const chartRef = React.createRef();
+  React.useEffect(() => {
+    const myChart = new Chart(chartRef.current, {
+      type: "line",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      }
+    });
+  }, [chartRef]);
 
-          <Toast show={showToast} onClose={toggleShowA}>
-            <Toast.Header>
-              <strong className="mr-auto">Bootstrap</strong>
-              <small>11 mins ago</small>
-            </Toast.Header>
-            <Toast.Body>Hello, {userInfo.name}.</Toast.Body>
-          </Toast>
-        </header>
-      </div>
-    </>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <HelloWorld userInfo={userInfo} hiFn={hi} />
+      </header>
+      <canvas ref={chartRef} width="400" height="400"></canvas>
+    </div>
   );
 }
